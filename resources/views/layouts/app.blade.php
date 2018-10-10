c69517ebe6c2f6a8c4fa3b743f4222b989aba956
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-09 23:43:45 -0400

    added navbar, some includes for index, npm installed popper.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
+      @include('includes.navbar')
       @yield('content')
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>{{$title}}</h1>
-  <p>Hurricane Micahel is going to screw up the Florida Panhandle in 2018</p>
+  @include('includes.index.jumbo')
+  <div class="container">
+    <h1>{{$title}}</h1>
+    <p>Hurricane Micahel is going to screw up the Florida Panhandle in 2018</p>
+  </div>
 @endsection
