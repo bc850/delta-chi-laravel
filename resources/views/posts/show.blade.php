4b33a7ee57dcdab76a488bfa2e49bd5ef0a14adc
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-12 19:23:25 -0400

    edited posts show page to include a delete button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,11 @@
         </div>
         <div class="card-footer">
           <a href="/posts" class="btn btn-default">Back</a>
-          <a href="/posts/{{$post->id}}/edit" class="btn btn-success">Edit</a>
+          <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+          {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+          {!!Form::close()!!}
         </div>
       </div>
     </div>
