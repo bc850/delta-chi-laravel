760a7440e51cbba93240d5b0386478e5a9e35cd3
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-12 01:48:30 -0400

    added messages to layout/app, added store logic for Post in controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
     </head>
     <body>
       @include('includes.navbar')
+      <div class="container mt-3">
+        @include('includes.messages')
+      </div>
       @yield('content')
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
