7d6f0f1b02ea8050d7e367fc0c8a60156a085b48
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-12 01:56:41 -0400

    updates to navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,43 +1,45 @@
 <header>
   <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark px-5">
-    <a class="navbar-brand" href="/"><img src="https://s3.amazonaws.com/delta-chi/deltachilogo.png" style="width: 175px; height: auto;" /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/posts">Posts</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            About
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item " href="/values">Delta Chi Values</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item " href="#">Advocate</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item " href="#">Volunteer</a>
-          </div>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right mr-3">
-        <li class="nav-item">
-          <a class="nav-link" href="/posts/create">Create Post</a>
-        </li>
-      </ul>
-      <form class="form-inline mt-2 mt-md-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/"><img src="https://s3.amazonaws.com/delta-chi/deltachilogo.png" style="width: 175px; height: auto;" /></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts">Posts</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              About
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item " href="/values">Delta Chi Values</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item " href="#">Advocate</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item " href="#">Volunteer</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right mr-3">
+          <li class="nav-item">
+            <a class="nav-link" href="/posts/create">Create Post</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   </nav>
 </header>
