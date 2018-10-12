265b97a5c53e3e1c6490980eba92604e0f7bbc24
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-11 23:33:47 -0400

    edited posts/index to add posts, added Post model to PostsController

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
   <div class="container mt-3">
-    <h3>Index for Posts</h3>
+    <h2>Index for Posts</h2>
+    @if(count($posts) > 1)
+      <div class="row">
+        @foreach($posts as $post)
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-title">
+                <h3>{{$post->title}}
+              </div>
+              <div class="card-body">
+                <p>{{$post->body}}</p>
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    @else
+      <p>No posts found.</p>
+    @endif
   </div>
 @endsection
