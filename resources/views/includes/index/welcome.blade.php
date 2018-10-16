9dbdfa4761e4352f883368b5c5d951c9e14a0734
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-16 13:06:25 -0400

    added oswald font, styling for includes

---
+++ resources/views/includes/index/welcome.blade.php
@@ -0,0 +1,24 @@
+<div class="container p18">
+  <!--<h1>{{$title}}</h1>-->
+  <h1 class="text-center">Valdosta State University Delta Chi Chapter</h1>
+  <p>
+    The Valdosta State University Chapter of The Delta Chi Fraternity was
+    established in 1968 as one of the first fraternities on campus. Our
+    Brotherhood celebrates hard work, a commitment to excellence, and the
+    camaraderie found through shared, common values:
+  </p>
+  <p class="text-center">
+    <strong>FRIENDSHIP. CHARACTER. JUSTICE. EDUCATION.</strong>
+  </p>
+  <p>
+    Our Chapter is involved in many activities surrounding the University and
+    our local community. We are involved in multiple philanthropic organizations,
+    University intramural sports, fraternity and sorority greek life events,
+    and have a strong network of alumni locally and in the Metro-Atlanta area
+    that can support you in the future.
+  </p>
+  <p>
+    Simply put, being a Delta Chi at Valdosta State University means you are
+    going to have the resources and opportunities to be more successful in life.
+  </p>
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
   @include('includes.index.jumbo')
-  <div class="container">
-    <h1>{{$title}}</h1>
-    <p>Hurricane Micahel is going to screw up the Florida Panhandle in 2018</p>
-  </div>
+  @include('includes.index.welcome')
+  @include('includes.index.powerglory')
 @endsection
