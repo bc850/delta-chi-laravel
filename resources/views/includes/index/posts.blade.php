6894be078f2344f68c54b04fa795500845468de1
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-18 20:24:17 -0400

    updates to includes/index/posts

--- resources/views/includes/index/posts.blade.php
+++ resources/views/includes/index/posts.blade.php
@@ -6,18 +6,18 @@
   </div>
   <div class="row">
     @foreach ($posts as $post)
-      <div class="col-md-4 text-center">
+      <div class="col-lg-4 text-center">
         <div class="card">
           <div class="card-header" style="background-image: url({{$post->image_url}});"></div>
           <div class="card-body">
-            <div class="card-title">
-              <h5>{{$post->title}}</h5>
+            <div class="card-title mt-4">
+              <h5>{{ str_limit($post->title, $limit = 67, $end = '...') }}</h5>
             </div>
-            <div class="card-text">
-              <p>{{ str_limit($post->blurb, $limit = 10, $end = '...') }}</p>
+            <div class="card-text mt-3">
+              <p>{{ str_limit($post->blurb, $limit = 140, $end = '...') }}</p>
             </div>
-            <div class="button-container">
-              <button type="button" class="btn btn-primary" href="#" id="learnMoreButton">Learn More</button>
+            <div class="button-container mt-3">
+              <a class="btn btn-primary" href="/posts/{{$post->id}}" id="learnMoreButton">Read More</a>
             </div>
           </div>
         </div>
