b095f06c94e26c529a8984fde4882c55b6593c88
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-27 01:36:01 -0400

    added social media feeds partial to all philanthropy and about pages

--- resources/views/pages/about/values.blade.php
+++ resources/views/pages/about/values.blade.php
@@ -144,14 +144,7 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4">
-        <div class="col-md-12" style="background-color: #fff; border: 1px solid #eee;">
-          <a class="twitter-timeline" data-height="450" href="https://twitter.com/VSUDeltaChi?ref_src=twsrc%5Etfw">Tweets by VSUDeltaChi</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-        <div class="col-md-12 mt-3" style="background-color: #fff; border: 1px solid #eee;">
-          <a class="twitter-timeline" data-height="450" href="https://twitter.com/DeltaChiHQ?ref_src=twsrc%5Etfw">Tweets by DeltaChiHQ</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
+      @include('includes.social-media-feeds')
     </div>
   </div>
 @endsection
